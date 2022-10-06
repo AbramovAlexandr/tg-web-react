f71645fd218d30534433c3e9f9eaeda4b536bcda
AbramovAlexandr <abramovsashok@mail.ru>
2022-10-06 13:44:09 +0500

    ch9

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import { useTelegram } from './hooks/useTelegram';
 
 function App() {
 
-  const {onToggleButton, tg } = useTelegram()
+  const {tg } = useTelegram()
   useEffect( () => {
     tg.ready();
   }, [])
+
   return (
     <div className="App">
       <Header/>
